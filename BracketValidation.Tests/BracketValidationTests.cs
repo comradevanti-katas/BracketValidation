cce3b822c6713c53b44f034a69bb9f9a23941b39
Ramon Brullo <kingvanti650@gmail.com>
2023-02-28 11:45:10 +0100

    test: add empty string test

---
+++ BracketValidation.Tests/BracketValidationTests.cs
@@ -0,0 +1,14 @@
+﻿namespace BracketValidation;
+using static BracketValidation;
+
+public class BracketValidationTests
+{
+
+    [Fact]
+    public void Empty_String_Is_Valid()
+    {
+        var isValid = IsValid("");
+        Assert.True(isValid);
+    }
+    
+}
